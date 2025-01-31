8a4b7b3f12ebac222dded2fab5cb963829157ddd
Ravendra Patel <pawan.developers@gmail.com>
2025-01-31 19:59:21 +0530

    Update pwn-bestseller-block.php

--- pwn-bestseller-block.php
+++ pwn-bestseller-block.php
@@ -40,3 +40,53 @@ function pwn_bestseller_block_init() {
     ));
 }
 add_action('init', 'pwn_bestseller_block_init');
+
+
+
+
+// Add API endpoint for genre fetching
+function pwn_register_genre_route() {
+    register_rest_route('pwn/v1', '/genres', array(
+        'methods' => 'GET',
+        'callback' => 'pwn_get_genres',
+        'permission_callback' => '__return_true'
+    ));
+    
+    register_rest_route('pwn/v1', '/bestseller', array(
+        'methods' => 'GET',
+        'callback' => 'pwn_get_bestseller',
+        'permission_callback' => '__return_true'
+    ));
+}
+add_action('rest_api_init', 'pwn_register_genre_route');
+
+function pwn_get_genres() {
+    $api_key = '7fqge2qgxcdrwqbcgeywwdj2';
+    $response = wp_remote_get(
+        'https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.UK/categories?rows=15&catSetId=PW&api_key=' . $api_key
+    );
+    
+    if (is_wp_error($response)) {
+        return new WP_Error('api_error', 'Failed to fetch genres', array('status' => 500));
+    }
+    
+    $body = json_decode(wp_remote_retrieve_body($response), true);
+    return rest_ensure_response($body);
+}
+
+function pwn_get_bestseller($request) {
+    $api_key = '7fqge2qgxcdrwqbcgeywwdj2';
+    $genre_id = $request->get_param('genreId');
+    
+    $response = wp_remote_get(
+        'https://api.penguinrandomhouse.com/resources/v2/title/domains/PRH.UK/works/views/uk-list-display?' .
+        'rows=1&catUri=' . $genre_id . '&catSetId=PW&sort=weeklySales&dir=desc&api_key=' . $api_key
+    );
+    
+    if (is_wp_error($response)) {
+        return new WP_Error('api_error', 'Failed to fetch bestseller', array('status' => 500));
+    }
+    
+    $body = json_decode(wp_remote_retrieve_body($response), true);
+    return rest_ensure_response($body);
+}
